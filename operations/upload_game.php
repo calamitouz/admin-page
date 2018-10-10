ace7d7cf73acac5d9e5009303b47247cb520882e
wloody <wloody1996@gmail.com>
2018-10-10 23:35:31 +0300

    5

---
+++ operations/upload_game.php
@@ -0,0 +1,17 @@
+<?php
+
+$con = mysqli_connect('localhost', 'root', 'root', 'yogames');
+
+
+$game_name = $_POST['game_name'];
+$game_tags = $_POST['game_tags'];
+$classification  = $_POST['classification'];
+
+$insert_query = "INSERT INTO games_list (game_name, tags, classification) VALUE ('$game_name', '$game_tags', '$classification')";
+$insert_query_result = mysqli_query($con, $insert_query);
+
+if($insert_query_result){
+    echo "done";
+}else{
+    echo "error";
+}
